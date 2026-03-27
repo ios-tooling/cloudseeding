16c52ead007e21e401770c9c71484d9702c9d254
Ben Gottlieb <ben@standalone.com>
2026-03-26 21:08:09 -0500

    added CKRecord.approximateSize

--- Sources/CloudSeeding/CKRecord.swift
+++ Sources/CloudSeeding/CKRecord.swift
@@ -7,10 +7,36 @@
 
 import Foundation
 import CloudKit
+import CoreLocation
 
 public extension CKRecord {
 	var createdByRecordID: String? {
 		let name = self.creatorUserRecordID?.recordName
 		return name == CKCurrentUserDefaultName ? CloudKitInterface.currentUserID : name
+	}
+
+	var approximateSize: Int {
+		allKeys().reduce(0) { total, key in
+			total + approximateSize(of: self[key])
+		}
+	}
+
+	private func approximateSize(of value: CKRecordValue?) -> Int {
+		switch value {
+		case let string as String: return string.utf8.count
+		case let data as Data: return data.count
+		case is NSNumber: return MemoryLayout<Double>.size
+		case is Date: return MemoryLayout<Date>.size
+		case let asset as CKAsset:
+			guard let url = asset.fileURL,
+					let attrs = try? FileManager.default.attributesOfItem(atPath: url.path),
+					let size = attrs[.size] as? Int else { return 0 }
+			return size
+		case let array as [CKRecordValue]:
+			return array.reduce(0) { $0 + approximateSize(of: $1) }
+		case is CLLocation: return MemoryLayout<CLLocationCoordinate2D>.size
+		case let reference as CKRecord.Reference: return reference.recordID.recordName.utf8.count
+		default: return 0
+		}
 	}
 }
